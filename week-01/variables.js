fad961dba3f7904c7ffb3048681ecb06084713bb
muharram20 <muharramabobakr@gmail.com>
2026-01-10 01:28:17 +0200

    Week 1 Day 1: variables and console.log

--- week-01/variables.js
+++ week-01/variables.js
@@ -1,7 +1,7 @@
 let age = 21;
 let city = "Cairo";
 let hobby = "football";
-let fullName = "Muharram Abobakr";
+let fullName = " Muharram Abobakr";
 
 console.log("Age:", age);
 console.log("City:", city);
