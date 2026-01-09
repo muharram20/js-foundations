0eb1c72e6a8892f6b65c670d49557e13d8059b44
muharram20 <muharramabobakr@gmail.com>
2026-01-10 01:18:43 +0200

    Week 1 Day 2: strings and string methods

---
+++ week-01/string.js
@@ -0,0 +1,19 @@
+let firstname = "Muharram";
+let secondname ="Abobakr";
+
+let fullname = firstname + " " + secondname;
+
+console.log(fullname);
+
+
+let name = 'Ahmed';
+name +=   ' Ali'
+
+console.log(name);
+
+let string1= 'Hi';
+let string2= 'nice to see you'
+let string3='How can I help you?'
+
+let all= string1.concat(', ', string2, '. ' , string3)
+console.log(all);
