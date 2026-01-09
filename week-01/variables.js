c7bed65c7040e1be9dc7da4f6c38742a707aa041
muharram20 <muharramabobakr@gmail.com>
2026-01-09 02:57:40 +0200

    feat: add week 1 variables practice

---
+++ week-01/variables.js
@@ -0,0 +1,9 @@
+let age = 21;
+let city = "Cairo";
+let hobby = "football";
+let fullName = "Muharram Abobakr";
+
+console.log("Age:", age);
+console.log("City:", city);
+console.log("Hobby:", hobby);
+console.log("Name:", fullName);
