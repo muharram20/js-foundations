6d81d4cc245918de0beff3c62c1e21531d13b6fb
muharram20 <muharramabobakr@gmail.com>
2026-02-01 18:35:07 +0200

    Delete variables.js

--- variables.js
+++
@@ -1,9 +0,0 @@
-let age = 21;
-let city = "Cairo";
-let hobby = "football";
-let fullName = "Muharram Abobakr";
-
-console.log("Age:", age);
-console.log("City:", city);
-console.log("Hobby:", hobby);
-console.log("Name:", fullName);
