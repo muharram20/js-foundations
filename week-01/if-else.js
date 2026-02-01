40951cdba20b9e8bac189a6d66c321df71f72ebd
muharram20 <muharramabobakr@gmail.com>
2026-02-01 19:05:49 +0200

    basic if-else statement

---
+++ week-01/if-else.js
@@ -0,0 +1,17 @@
+const score = 89;
+ 
+if(score>=90){
+    console.log('your got A');
+}
+else if(score>=80){
+    console.log('you got B');
+}
+ else if(score>=70){
+console.log('you got C');
+}
+else if(score>=60){
+    console.log('you got D');
+}
+else{
+    console.log('you got F');
+}
