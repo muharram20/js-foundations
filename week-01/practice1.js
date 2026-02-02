7268d976ccb60de1c969771b1bfff79ed43ecba1
muharram20 <muharramabobakr@gmail.com>
2026-02-02 20:53:52 +0200

    if-else practice programs

---
+++ week-01/practice1.js
@@ -0,0 +1,8 @@
+let age = 19;
+
+if(age>=21 ){
+    console.log("you can drink");
+}
+else{
+    console.log("you cannot drink");
+}
---
+++ week-01/practice2.js
@@ -0,0 +1,17 @@
+let num = 21;
+
+if(num % 2 === 0){
+    console.log("the number is even");
+}
+else{
+    console.log("the number is odd");
+}
+
+let country = "Eygpt";
+
+if (country === "Eygpt" ){
+    console.log("welcome home"); 
+}
+else{
+    console.log("welcome visitor");
+}
